d0854e616b03746e7bb4acf8f8cc3e5cd89da026
Supun <supunsnnk@gmail.com>
2018-04-12 03:40:15 +0530

    final

--- js/search.js
+++ js/search.js
@@ -4,12 +4,16 @@ var app = angular
     .controller("myController", function ($scope) {
 
         var employees = [
-            { name: "Ben", gender: "Male", salary: 55000, city: "London" },
-            { name: "Sara", gender: "Female", salary: 68000, city: "Chennai" },
-            { name: "Mark", gender: "Male", salary: 57000, city: "London" },
-            { name: "Pam", gender: "Female", salary: 53000, city: "Chennai" },
-            { name: "Todd", gender: "Male", salary: 60000, city: "London" },
+            { username: "Ben",business_name:"Ben12", gender: "Male", category : "Accessories", town_city: "London",state:"",about:"" },
+            { username: "Supun",business_name:"supunAbc", gender: "female ", category : "Accomodation", town_city: "Matara",state:"",about:"" },
+            { username: "Camal",business_name:"camal", gender: "Male", category : "Actor", town_city: "Colombo",state:"",about:"" },
+            { username: "Nimal",business_name:"Nimalsnnk", gender: "female ", category : "Actor", town_city: "Dabulla",state:"",about:"" },
+            { username: "Pasidu",business_name:"Pasidu123", gender: "Male", category : "Accountants & Auditors", town_city: "London",state:"",about:"" },
+            {username: "Jon",business_name:"silva", gender: "Male", category : "Accessories", town_city: "London",state:"",about:"" },
+            {username: "Jon",business_name:"silva", gender: "Male", category : "Accountant", town_city: "New York",state:"",about:"" },
         ];
 
         $scope.employees = employees;
+
+        $scope.Categories = [{id: 1,name: 'Accessories'}, {id: 2,name: 'Accomodation'},{id: 3,name: 'Accountant'}, {id: 4,name: 'Accountants & Auditors'},{id: 5,name: 'Activewear'}, {id: 6,name: 'Actor'}];
     });
